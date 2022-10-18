f666b7a6d2732c82b7a7af0a9c6d7d9f6578712e
Simon Moses <simon@seamlesshr.com>
2022-10-18 18:05:36 +0100

    installed carbon design system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button } from '@carbon/react';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button kind="danger">Button1</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
